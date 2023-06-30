0c374260266147f5b80e6b8a992b9d05342bec74
Igor Vicente <compass-igor.machado@ailos.coop.br>
2023-06-29 22:03:46 -0300

    inicial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProjetoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 1;
+            var c = 1;
+
+            System.Console.WriteLine(a+b+c);
+        }
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Testing Advanced Commands on Git
